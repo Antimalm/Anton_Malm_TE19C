a992e0cead9d111925ae3b547e7d7993c4b46c12
Antimalm <anton.malm@elev.ga.ntig.se>
2020-09-11 09:39:12 +0200

    nytt

---
+++ id-satser1.py
@@ -0,0 +1,24 @@
+# jämt eller udda
+tal = int(input ("Ange ett heltal:"))
+
+
+print(tal%2) 
+if tal%2 == 1:
+    print("0jämt tal")
+else:
+    print("Jämnt tal")
+
+vinkel = float(input("Ange en vinkel v i grader där 0 <= v<=360  "))
+#spetsig: 0<v<90
+#rät: v=90
+#trubbig: 90<v<180
+
+if vinkel >=0 and vinkel < 90: 
+    print("spetsig vinkel.")
+elif vinkel == 90:
+    print("rät vinkel")
+elif vinkel > 90 and vinkel <180:
+    print("Trubbig")
+else:  
+    print("vinkeln är inte spetsig, rät eller trubbig")
+    
