ab926c288b84782bfde9a07086d3974ee89cdabe
Antimalm <anton.malm@elev.ga.ntig.se>
2020-09-16 13:38:03 +0200

    upgift 1 och 2 klar

--- Laxa2/laxa2.py
+++ Laxa2/laxa2.py
@@ -5,5 +5,15 @@ if t<0:
     print("talet är negativt")
 elif t>0: 
     print("talet är positivt")
-else 
+else: 
     print: ("talet är 0")
+
+#upg 2.
+t = float(input("Skriv vilket tal du vill"))
+if t%2 == 1:    
+    print("talet är ojämnt")
+else:
+    print("talet är jämnt")
+if t%5 ==0:
+    print("Talet är delbart med 5")
+
