f04aa33ca122ab25bca0601f91def34da63ad44b
Antimalm <anton.malm@elev.ga.ntig.se>
2020-09-30 14:02:18 +0200

    några uppgifter klara

---
+++ Laxa2/laxa3_for.py
@@ -0,0 +1,54 @@
+#for i in range(1,101):
+    #print (i, end=" ")
+
+#for k in range(50,101):
+    #print(k, end=" ")
+
+#for j in range(100,49,-1):
+   # print(j ,end=" ")
+
+#s=0
+#for i in range(11):
+  #  s+= i
+#print (f"1+2+3...+9.10 = {s}")
+#1afor i in range(1,11):
+    #print(float(i))
+
+
+#1bfor i in range(-10,11):
+    #print(i)
+
+#1cfor i in range (10,-1,-1):
+    #print(i)
+
+#2for n in range(1,101):
+
+    #2for a in range(2,n):
+        #if (n%a == 0):
+            #break
+    #else:
+        #print(n, "primtal")   
+
+#upg 3 
+# s = 0
+#for i in range(1,100):
+    #s +=i
+#print (s)
+
+#upg 4
+# s = 0
+#for i in range(1,100,2):
+    #s +=i
+#print (s)
+'''
+upgift 5 a
+for i in range (0,11):
+    print(f"5*{i} = {i*5}")
+''' 
+
+
+
+
+t=int(input("Skriv ett tal mellan 0-10"))
+for i in range (0,11):
+    print(f"{t*i}= {i*t}")
---
+++ nästlad_for.py
@@ -0,0 +1,5 @@
+for i in range (0,11):
+    print(f"5*{i} = {i*5}")
+
+
+
